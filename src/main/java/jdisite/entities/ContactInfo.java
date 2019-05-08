a3bf07a79b668f7f52b63c4cf3c09eb136c3f27c
RomanIovlev <Roman_Iovlev@epam.com>
2019-05-09 00:04:54 +0300

    add int and boolean in ContactInfo

--- src/main/java/jdisite/entities/ContactInfo.java
+++ src/main/java/jdisite/entities/ContactInfo.java
@@ -3,6 +3,7 @@ package jdisite.entities;
 import com.epam.jdi.tools.DataClass;
 
 public class ContactInfo extends DataClass<ContactInfo> {
-    public String passport, name, lastName, position, passportNumber,
-        passportSeria, gender, religion, weather, acceptConditions, description;
+    public String name, lastName, position, gender, religion, weather, description;
+    public int passportNumber, passportSeria;
+    public boolean acceptConditions, passport;
 }
